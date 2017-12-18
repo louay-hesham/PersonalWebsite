820bf19ba783a0e8d12ad3427efff7dbe98beadc
Louay Morsi <louay.hesham@gmail.com>
2017-12-18 18:45:21 +0200

    Moved work experience to separate module

--- src/app/quick-info/quick-info.component.ts
+++ src/app/quick-info/quick-info.component.ts
@@ -12,7 +12,6 @@ export class QuickInfoComponent implements OnInit {
 	public awardsVisible: boolean;
 	public projectsVisible: boolean;
 	public skillsVisible: boolean;
-	public contactVisible: boolean;
 
 	public visibility: boolean[] = [];
 
@@ -22,20 +21,18 @@ export class QuickInfoComponent implements OnInit {
   	this.awardsVisible = false;
   	this.projectsVisible = false;
   	this.skillsVisible = false;
-  	this.contactVisible = false;
   	this.visibility.push(this.workExperienceVisible);
   	this.visibility.push(this.educationVisible);
   	this.visibility.push(this.awardsVisible);
   	this.visibility.push(this.projectsVisible);
   	this.visibility.push(this.skillsVisible);
-  	this.visibility.push(this.contactVisible);
   }
 
   ngOnInit() { }
 
   toggle(v) {
   	var i:number;
-  	for (i = 0; i < 6; i++){
+  	for (i = 0; i < 5; i++){
   		if (i != v)
   			this.visibility[i] = false;
   	}
@@ -45,7 +42,7 @@ export class QuickInfoComponent implements OnInit {
   checkVisibility(v){
   	var visible: boolean = true
   	var i:number;
-  	for (i = 0; i < 6; i++){
+  	for (i = 0; i < 5; i++){
   		if (i != v)
   			visible = visible & !this.visibility[i];
 			else 
